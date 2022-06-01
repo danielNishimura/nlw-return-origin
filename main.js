217b17b1ec59537e551eb7e03a972b475a76a2bb
danielNishimura <nishimurani@gmail.com>
2022-05-31 22:17:31 -0300

    continuação aula 5

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 window.addEventListener('scroll', onScroll)
 
+onScroll()
 function onScroll() {
   showNavOnScroll(),
   showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  // activateMenuAtCurrentSection(services)
+}
+
+function activeMenuAtCurrentSection(section) {
+  //linha alvo
+  const targetLine = scrollY + innerHeight / 2
+  //verificar se a seção passou da linha
+  // quais dasdos vou precisar?
+
+  // o topo da seção
+  const sectionTop = home.offsetTop
+
+  // a altura da seção
+  const sectionHeight = home.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // informações dos dados e da lógica
+  console.log(
+    'O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetLine
+  )
+
+  //verificar se a base está abaico da linha alvo
+
+  // quais dados vou precisar?
+
+  //a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da seção passou da linha alvo
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  console.log('O fundo da seção passou da linha?', sectionEndPassedTargetline)
+
 }
 
 function showNavOnScroll() {
